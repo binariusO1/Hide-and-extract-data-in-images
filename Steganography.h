ed31033b7654a02f17b722dd8b02687ec60ed17c
binariusO1 <ryszard1191@gmail.com>
2020-05-21 19:50:27 +0200

    up

--- Steganography.h
+++ Steganography.h
@@ -1,5 +1,14 @@
 #pragma once
+
+#include <iostream>
+#include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/imgproc.hpp>
+#include <fstream>							//to save file
+
 ref class Steganography
 {
+
 };
 

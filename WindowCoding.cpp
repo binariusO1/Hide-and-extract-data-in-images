1ff7f1bf3d61509502e3fa2533f722b192e047e1
binariusO1 <ryszard1191@gmail.com>
2020-05-21 16:52:59 +0200

    up

--- WindowCoding.cpp
+++ WindowCoding.cpp
@@ -18,3 +18,236 @@ int app_WindowCoding()
 	return 0;
 }
 */
+using namespace Hideandextractdatainimages;
+using namespace System;
+
+//************************************************************
+//		WINDOW FORM ELEMENTS
+//************************************************************
+void WindowCoding::InitializeComponent(void)
+{
+	this->components = (gcnew System::ComponentModel::Container());
+	System::ComponentModel::ComponentResourceManager^ resources = (gcnew System::ComponentModel::ComponentResourceManager(WindowCoding::typeid));
+	this->tabControl1 = (gcnew System::Windows::Forms::TabControl());
+	this->tabPage1 = (gcnew System::Windows::Forms::TabPage());
+	this->C_button_ResProcess = (gcnew System::Windows::Forms::Button());
+	this->C_button_Process = (gcnew System::Windows::Forms::Button());
+	this->C_groupBox_Hide = (gcnew System::Windows::Forms::GroupBox());
+	this->C_label_Height_Hide = (gcnew System::Windows::Forms::Label());
+	this->C_label_Width_Hide = (gcnew System::Windows::Forms::Label());
+	this->C_listBox_Hide = (gcnew System::Windows::Forms::ListBox());
+	this->C_groupBox_Main = (gcnew System::Windows::Forms::GroupBox());
+	this->C_label_Height_Main = (gcnew System::Windows::Forms::Label());
+	this->C_label_Width_Main = (gcnew System::Windows::Forms::Label());
+	this->C_listBox_Main = (gcnew System::Windows::Forms::ListBox());
+	this->tabPage2 = (gcnew System::Windows::Forms::TabPage());
+	this->D_button_Process = (gcnew System::Windows::Forms::Button());
+	this->D_groupBox = (gcnew System::Windows::Forms::GroupBox());
+	this->D_label_Height = (gcnew System::Windows::Forms::Label());
+	this->D_label_Width = (gcnew System::Windows::Forms::Label());
+	this->D_listBox = (gcnew System::Windows::Forms::ListBox());
+	this->toolTip1 = (gcnew System::Windows::Forms::ToolTip(this->components));
+	this->tabControl1->SuspendLayout();
+	this->tabPage1->SuspendLayout();
+	this->C_groupBox_Hide->SuspendLayout();
+	this->C_groupBox_Main->SuspendLayout();
+	this->tabPage2->SuspendLayout();
+	this->D_groupBox->SuspendLayout();
+	this->SuspendLayout();
+	// 
+	// tabControl1
+	// 
+	this->tabControl1->AllowDrop = true;
+	this->tabControl1->Controls->Add(this->tabPage1);
+	this->tabControl1->Controls->Add(this->tabPage2);
+	resources->ApplyResources(this->tabControl1, L"tabControl1");
+	this->tabControl1->Name = L"tabControl1";
+	this->tabControl1->SelectedIndex = 0;
+	this->tabControl1->SizeMode = System::Windows::Forms::TabSizeMode::Fixed;
+	// 
+	// tabPage1
+	// 
+	this->tabPage1->Controls->Add(this->C_button_ResProcess);
+	this->tabPage1->Controls->Add(this->C_button_Process);
+	this->tabPage1->Controls->Add(this->C_groupBox_Hide);
+	this->tabPage1->Controls->Add(this->C_groupBox_Main);
+	resources->ApplyResources(this->tabPage1, L"tabPage1");
+	this->tabPage1->Name = L"tabPage1";
+	this->tabPage1->UseVisualStyleBackColor = true;
+	// 
+	// C_button_ResProcess
+	// 
+	resources->ApplyResources(this->C_button_ResProcess, L"C_button_ResProcess");
+	this->C_button_ResProcess->Name = L"C_button_ResProcess";
+	this->C_button_ResProcess->UseVisualStyleBackColor = true;
+	// 
+	// C_button_Process
+	// 
+	resources->ApplyResources(this->C_button_Process, L"C_button_Process");
+	this->C_button_Process->Name = L"C_button_Process";
+	this->C_button_Process->UseVisualStyleBackColor = true;
+	// 
+	// C_groupBox_Hide
+	// 
+	this->C_groupBox_Hide->Controls->Add(this->C_label_Height_Hide);
+	this->C_groupBox_Hide->Controls->Add(this->C_label_Width_Hide);
+	this->C_groupBox_Hide->Controls->Add(this->C_listBox_Hide);
+	resources->ApplyResources(this->C_groupBox_Hide, L"C_groupBox_Hide");
+	this->C_groupBox_Hide->Name = L"C_groupBox_Hide";
+	this->C_groupBox_Hide->TabStop = false;
+	// 
+	// C_label_Height_Hide
+	// 
+	resources->ApplyResources(this->C_label_Height_Hide, L"C_label_Height_Hide");
+	this->C_label_Height_Hide->Name = L"C_label_Height_Hide";
+	// 
+	// C_label_Width_Hide
+	// 
+	resources->ApplyResources(this->C_label_Width_Hide, L"C_label_Width_Hide");
+	this->C_label_Width_Hide->Name = L"C_label_Width_Hide";
+	// 
+	// C_listBox_Hide
+	// 
+	this->C_listBox_Hide->AllowDrop = true;
+	this->C_listBox_Hide->Cursor = System::Windows::Forms::Cursors::Hand;
+	this->C_listBox_Hide->FormattingEnabled = true;
+	resources->ApplyResources(this->C_listBox_Hide, L"C_listBox_Hide");
+	this->C_listBox_Hide->Name = L"C_listBox_Hide";
+	this->toolTip1->SetToolTip(this->C_listBox_Hide, resources->GetString(L"C_listBox_Hide.ToolTip"));
+	// 
+	// C_groupBox_Main
+	// 
+	this->C_groupBox_Main->Controls->Add(this->C_label_Height_Main);
+	this->C_groupBox_Main->Controls->Add(this->C_label_Width_Main);
+	this->C_groupBox_Main->Controls->Add(this->C_listBox_Main);
+	resources->ApplyResources(this->C_groupBox_Main, L"C_groupBox_Main");
+	this->C_groupBox_Main->Name = L"C_groupBox_Main";
+	this->C_groupBox_Main->TabStop = false;
+	// 
+	// C_label_Height_Main
+	// 
+	resources->ApplyResources(this->C_label_Height_Main, L"C_label_Height_Main");
+	this->C_label_Height_Main->Name = L"C_label_Height_Main";
+	// 
+	// C_label_Width_Main
+	// 
+	resources->ApplyResources(this->C_label_Width_Main, L"C_label_Width_Main");
+	this->C_label_Width_Main->Name = L"C_label_Width_Main";
+	// 
+	// C_listBox_Main
+	// 
+	this->C_listBox_Main->AllowDrop = true;
+	this->C_listBox_Main->Cursor = System::Windows::Forms::Cursors::Hand;
+	this->C_listBox_Main->FormattingEnabled = true;
+	resources->ApplyResources(this->C_listBox_Main, L"C_listBox_Main");
+	this->C_listBox_Main->Name = L"C_listBox_Main";
+	this->C_listBox_Main->SelectionMode = System::Windows::Forms::SelectionMode::None;
+	this->toolTip1->SetToolTip(this->C_listBox_Main, resources->GetString(L"C_listBox_Main.ToolTip"));
+	this->C_listBox_Main->DragDrop += gcnew System::Windows::Forms::DragEventHandler(this, &WindowCoding::listBox1_DragDrop);
+	this->C_listBox_Main->DragEnter += gcnew System::Windows::Forms::DragEventHandler(this, &WindowCoding::listBox1_DragEnter);
+	// 
+	// tabPage2
+	// 
+	this->tabPage2->Controls->Add(this->D_button_Process);
+	this->tabPage2->Controls->Add(this->D_groupBox);
+	resources->ApplyResources(this->tabPage2, L"tabPage2");
+	this->tabPage2->Name = L"tabPage2";
+	this->tabPage2->UseVisualStyleBackColor = true;
+	// 
+	// D_button_Process
+	// 
+	resources->ApplyResources(this->D_button_Process, L"D_button_Process");
+	this->D_button_Process->Name = L"D_button_Process";
+	this->D_button_Process->UseVisualStyleBackColor = true;
+	// 
+	// D_groupBox
+	// 
+	this->D_groupBox->Controls->Add(this->D_label_Height);
+	this->D_groupBox->Controls->Add(this->D_label_Width);
+	this->D_groupBox->Controls->Add(this->D_listBox);
+	resources->ApplyResources(this->D_groupBox, L"D_groupBox");
+	this->D_groupBox->Name = L"D_groupBox";
+	this->D_groupBox->TabStop = false;
+	// 
+	// D_label_Height
+	// 
+	resources->ApplyResources(this->D_label_Height, L"D_label_Height");
+	this->D_label_Height->Name = L"D_label_Height";
+	// 
+	// D_label_Width
+	// 
+	resources->ApplyResources(this->D_label_Width, L"D_label_Width");
+	this->D_label_Width->Name = L"D_label_Width";
+	// 
+	// D_listBox
+	// 
+	this->D_listBox->AllowDrop = true;
+	this->D_listBox->Cursor = System::Windows::Forms::Cursors::Hand;
+	this->D_listBox->FormattingEnabled = true;
+	resources->ApplyResources(this->D_listBox, L"D_listBox");
+	this->D_listBox->Name = L"D_listBox";
+	this->toolTip1->SetToolTip(this->D_listBox, resources->GetString(L"D_listBox.ToolTip"));
+	// 
+	// WindowCoding
+	// 
+	this->AllowDrop = true;
+	this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::None;
+	this->CausesValidation = false;
+	resources->ApplyResources(this, L"$this");
+	this->Controls->Add(this->tabControl1);
+	this->FormBorderStyle = System::Windows::Forms::FormBorderStyle::FixedSingle;
+	this->MaximizeBox = false;
+	this->Name = L"WindowCoding";
+	this->ShowIcon = false;
+	this->tabControl1->ResumeLayout(false);
+	this->tabPage1->ResumeLayout(false);
+	this->C_groupBox_Hide->ResumeLayout(false);
+	this->C_groupBox_Hide->PerformLayout();
+	this->C_groupBox_Main->ResumeLayout(false);
+	this->C_groupBox_Main->PerformLayout();
+	this->tabPage2->ResumeLayout(false);
+	this->D_groupBox->ResumeLayout(false);
+	this->D_groupBox->PerformLayout();
+	this->ResumeLayout(false);
+
+}
+
+System::Void WindowCoding::listBox1_DragDrop(System::Object^ sender, System::Windows::Forms::DragEventArgs^ e)
+{
+	/*
+	this->listBox1->Items->Clear();
+	cli::array<String^>^ files = (cli::array<String^>^)e->Data->GetData(DataFormats::FileDrop, false);
+	//String^ file;
+	for each (String ^ file in files)
+	{
+		if (files->Length == 1)
+		{
+			//MessageBox::Show("Check1: " + on_off + " ");
+			std::string s_file = msclr::interop::marshal_as<std::string>(file);
+
+
+			on_off = fbom_sprawdzenie(s_file);
+			String^ filename = bom_filename = Path::GetFileName(file);
+			bom_filepath = Path::GetDirectoryName(file);
+			this->listBox1->Items->Add(filename);
+			//MessageBox::Show("Filename: " + filename + " "+on_off+" ");
+		}
+
+		//THEN DO WHATEVER YOU WANT TO EACH file in files
+	}
+	*/
+}
+
+//************************************************************
+//		DRAG & DROP EVENTS
+//************************************************************
+System::Void WindowCoding::listBox1_DragEnter(System::Object^ sender, System::Windows::Forms::DragEventArgs^ e)
+{
+	if (e->Data->GetDataPresent(DataFormats::FileDrop, false) == true)
+	{
+		e->Effect = DragDropEffects::All;
+	}
+}
+System::Void WindowCoding::textBox1_TextChanged(System::Object^ sender, System::EventArgs^ e)
+{
+}
--- WindowCoding.h
+++ WindowCoding.h
@@ -12,275 +12,56 @@ namespace Hideandextractdatainimages {
 
 	public ref class WindowCoding : public System::Windows::Forms::Form
 	{
-	public:
-		WindowCoding(void)
-		{
-			InitializeComponent();
-		}
-
-	protected:
-		~WindowCoding()
-		{
-			if (components)
-			{
-				delete components;
+		public:
+			WindowCoding(void) {
+				InitializeComponent();
 			}
-		}
-	private: System::Windows::Forms::TabControl^ tabControl1;
-	private: System::Windows::Forms::TabPage^ tabPage1;
-	private: System::Windows::Forms::TabPage^ tabPage2;
-	private: System::Windows::Forms::ListBox^ listBox1;
-	private: System::Windows::Forms::GroupBox^ groupBox1;
-	private: System::Windows::Forms::Label^ label2;
-	private: System::Windows::Forms::Label^ label1;
-	private: System::Windows::Forms::ToolTip^ toolTip1;
-	private: System::Windows::Forms::Button^ button1;
-	private: System::Windows::Forms::GroupBox^ groupBox2;
-	private: System::Windows::Forms::Label^ label3;
-	private: System::Windows::Forms::Label^ label4;
-	private: System::Windows::Forms::ListBox^ listBox2;
-	private: System::Windows::Forms::Button^ button3;
-	private: System::Windows::Forms::Button^ button2;
-	private: System::Windows::Forms::GroupBox^ groupBox3;
-	private: System::Windows::Forms::Label^ label5;
-	private: System::Windows::Forms::Label^ label6;
-	private: System::Windows::Forms::ListBox^ listBox3;
-	private: System::ComponentModel::IContainer^ components;
+		protected:
+			~WindowCoding(){
+				if (components){
+					delete components;
+				}
+			}
 
-
-	private:
-
+//************************************************************
+//		WINDOW FORM ELEMENTS
+//************************************************************
+		private: 
+			System::Windows::Forms::TabControl^ tabControl1;
+			System::Windows::Forms::TabPage^ tabPage1;
+			System::Windows::Forms::TabPage^ tabPage2;
+			System::ComponentModel::IContainer^ components;
+//		C
+	private: System::Windows::Forms::ListBox^ C_listBox_Main;
+	private: System::Windows::Forms::GroupBox^ C_groupBox_Main;
+	private: System::Windows::Forms::Label^ C_label_Height_Main;
+	private: System::Windows::Forms::Label^ C_label_Width_Main;
+			System::Windows::Forms::ToolTip^ toolTip1;
+	private: System::Windows::Forms::Button^ C_button_Process;
+	private: System::Windows::Forms::GroupBox^ C_groupBox_Hide;
+	private: System::Windows::Forms::Label^ C_label_Height_Hide;
+	private: System::Windows::Forms::Label^ C_label_Width_Hide;
+	private: System::Windows::Forms::ListBox^ C_listBox_Hide;
+	private: System::Windows::Forms::Button^ C_button_ResProcess;
+//		D
+	private: System::Windows::Forms::Button^ D_button_Process;
+	private: System::Windows::Forms::GroupBox^ D_groupBox;
+	private: System::Windows::Forms::Label^ D_label_Height;
+	private: System::Windows::Forms::Label^ D_label_Width;
+	private: System::Windows::Forms::ListBox^ D_listBox;
 
 
 #pragma region Windows Form Designer generated code
+	private:
+		void InitializeComponent(void);
 
-		void InitializeComponent(void)
-		{
-			this->components = (gcnew System::ComponentModel::Container());
-			System::ComponentModel::ComponentResourceManager^ resources = (gcnew System::ComponentModel::ComponentResourceManager(WindowCoding::typeid));
-			this->tabControl1 = (gcnew System::Windows::Forms::TabControl());
-			this->tabPage1 = (gcnew System::Windows::Forms::TabPage());
-			this->tabPage2 = (gcnew System::Windows::Forms::TabPage());
-			this->listBox1 = (gcnew System::Windows::Forms::ListBox());
-			this->groupBox1 = (gcnew System::Windows::Forms::GroupBox());
-			this->label1 = (gcnew System::Windows::Forms::Label());
-			this->label2 = (gcnew System::Windows::Forms::Label());
-			this->toolTip1 = (gcnew System::Windows::Forms::ToolTip(this->components));
-			this->label3 = (gcnew System::Windows::Forms::Label());
-			this->label4 = (gcnew System::Windows::Forms::Label());
-			this->listBox2 = (gcnew System::Windows::Forms::ListBox());
-			this->groupBox2 = (gcnew System::Windows::Forms::GroupBox());
-			this->button1 = (gcnew System::Windows::Forms::Button());
-			this->groupBox3 = (gcnew System::Windows::Forms::GroupBox());
-			this->label5 = (gcnew System::Windows::Forms::Label());
-			this->label6 = (gcnew System::Windows::Forms::Label());
-			this->listBox3 = (gcnew System::Windows::Forms::ListBox());
-			this->button2 = (gcnew System::Windows::Forms::Button());
-			this->button3 = (gcnew System::Windows::Forms::Button());
-			this->tabControl1->SuspendLayout();
-			this->tabPage1->SuspendLayout();
-			this->tabPage2->SuspendLayout();
-			this->groupBox1->SuspendLayout();
-			this->groupBox2->SuspendLayout();
-			this->groupBox3->SuspendLayout();
-			this->SuspendLayout();
-			// 
-			// tabControl1
-			// 
-			this->tabControl1->AllowDrop = true;
-			this->tabControl1->Controls->Add(this->tabPage1);
-			this->tabControl1->Controls->Add(this->tabPage2);
-			resources->ApplyResources(this->tabControl1, L"tabControl1");
-			this->tabControl1->Name = L"tabControl1";
-			this->tabControl1->SelectedIndex = 0;
-			this->tabControl1->SizeMode = System::Windows::Forms::TabSizeMode::Fixed;
-			// 
-			// tabPage1
-			// 
-			this->tabPage1->Controls->Add(this->button3);
-			this->tabPage1->Controls->Add(this->button1);
-			this->tabPage1->Controls->Add(this->groupBox2);
-			this->tabPage1->Controls->Add(this->groupBox1);
-			resources->ApplyResources(this->tabPage1, L"tabPage1");
-			this->tabPage1->Name = L"tabPage1";
-			this->tabPage1->UseVisualStyleBackColor = true;
-			// 
-			// tabPage2
-			// 
-			this->tabPage2->Controls->Add(this->button2);
-			this->tabPage2->Controls->Add(this->groupBox3);
-			resources->ApplyResources(this->tabPage2, L"tabPage2");
-			this->tabPage2->Name = L"tabPage2";
-			this->tabPage2->UseVisualStyleBackColor = true;
-			// 
-			// listBox1
-			// 
-			this->listBox1->Name = L"listBox1";
-			this->listBox1->AllowDrop = true;
-			this->listBox1->Cursor = System::Windows::Forms::Cursors::Hand;
-			this->listBox1->FormattingEnabled = true;
-			resources->ApplyResources(this->listBox1, L"listBox1");
-			this->listBox1->SelectionMode = System::Windows::Forms::SelectionMode::None;
-			this->toolTip1->SetToolTip(this->listBox1, resources->GetString(L"listBox1.ToolTip"));
-			this->listBox1->DragDrop += gcnew System::Windows::Forms::DragEventHandler(this, &WindowCoding::listBox1_DragDrop);
-			this->listBox1->DragEnter += gcnew System::Windows::Forms::DragEventHandler(this, &WindowCoding::listBox1_DragEnter);
-			// 
-			// groupBox1
-			// 
-			this->groupBox1->Controls->Add(this->label2);
-			this->groupBox1->Controls->Add(this->label1);
-			this->groupBox1->Controls->Add(this->listBox1);
-			resources->ApplyResources(this->groupBox1, L"groupBox1");
-			this->groupBox1->Name = L"groupBox1";
-			this->groupBox1->TabStop = false;
-			// 
-			// label1
-			// 
-			resources->ApplyResources(this->label1, L"label1");
-			this->label1->Name = L"label1";
-			// 
-			// label2
-			// 
-			resources->ApplyResources(this->label2, L"label2");
-			this->label2->Name = L"label2";
-			// 
-			// label3
-			// 
-			resources->ApplyResources(this->label3, L"label3");
-			this->label3->Name = L"label3";
-			// 
-			// label4
-			// 
-			resources->ApplyResources(this->label4, L"label4");
-			this->label4->Name = L"label4";
-			// 
-			// listBox2
-			// 
-			this->listBox2->AllowDrop = true;
-			this->listBox2->Cursor = System::Windows::Forms::Cursors::Hand;
-			this->listBox2->FormattingEnabled = true;
-			resources->ApplyResources(this->listBox2, L"listBox2");
-			this->listBox2->Name = L"listBox2";
-			this->toolTip1->SetToolTip(this->listBox2, resources->GetString(L"listBox2.ToolTip"));
-			// 
-			// groupBox2
-			// 
-			this->groupBox2->Controls->Add(this->label3);
-			this->groupBox2->Controls->Add(this->label4);
-			this->groupBox2->Controls->Add(this->listBox2);
-			resources->ApplyResources(this->groupBox2, L"groupBox2");
-			this->groupBox2->Name = L"groupBox2";
-			this->groupBox2->TabStop = false;
-			// 
-			// button1
-			// 
-			resources->ApplyResources(this->button1, L"button1");
-			this->button1->Name = L"button1";
-			this->button1->UseVisualStyleBackColor = true;
-			// 
-			// groupBox3
-			// 
-			this->groupBox3->Controls->Add(this->label5);
-			this->groupBox3->Controls->Add(this->label6);
-			this->groupBox3->Controls->Add(this->listBox3);
-			resources->ApplyResources(this->groupBox3, L"groupBox3");
-			this->groupBox3->Name = L"groupBox3";
-			this->groupBox3->TabStop = false;
-			// 
-			// label5
-			// 
-			resources->ApplyResources(this->label5, L"label5");
-			this->label5->Name = L"label5";
-			// 
-			// label6
-			// 
-			resources->ApplyResources(this->label6, L"label6");
-			this->label6->Name = L"label6";
-			// 
-			// listBox3
-			// 
-			this->listBox3->AllowDrop = true;
-			this->listBox3->Cursor = System::Windows::Forms::Cursors::Hand;
-			this->listBox3->FormattingEnabled = true;
-			resources->ApplyResources(this->listBox3, L"listBox3");
-			this->listBox3->Name = L"listBox3";
-			this->toolTip1->SetToolTip(this->listBox3, resources->GetString(L"listBox3.ToolTip"));
-			// 
-			// button2
-			// 
-			resources->ApplyResources(this->button2, L"button2");
-			this->button2->Name = L"button2";
-			this->button2->UseVisualStyleBackColor = true;
-			// 
-			// button3
-			// 
-			resources->ApplyResources(this->button3, L"button3");
-			this->button3->Name = L"button3";
-			this->button3->UseVisualStyleBackColor = true;
-			// 
-			// WindowCoding
-			// 
-			this->AllowDrop = true;
-			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::None;
-			this->CausesValidation = false;
-			resources->ApplyResources(this, L"$this");
-			this->Controls->Add(this->tabControl1);
-			this->FormBorderStyle = System::Windows::Forms::FormBorderStyle::FixedSingle;
-			this->MaximizeBox = false;
-			this->Name = L"WindowCoding";
-			this->ShowIcon = false;
-			this->tabControl1->ResumeLayout(false);
-			this->tabPage1->ResumeLayout(false);
-			this->tabPage2->ResumeLayout(false);
-			this->groupBox1->ResumeLayout(false);
-			this->groupBox1->PerformLayout();
-			this->groupBox2->ResumeLayout(false);
-			this->groupBox2->PerformLayout();
-			this->groupBox3->ResumeLayout(false);
-			this->groupBox3->PerformLayout();
-			this->ResumeLayout(false);
-
-		}
-
-		//************************************************************
-//		DRAG & DROP
 //************************************************************
-
-	private: System::Void listBox1_DragDrop(System::Object^ sender, System::Windows::Forms::DragEventArgs^ e)
-	{
-		/*
-		this->listBox1->Items->Clear();
-		cli::array<String^>^ files = (cli::array<String^>^)e->Data->GetData(DataFormats::FileDrop, false);
-		//String^ file;
-		for each (String ^ file in files)
-		{
-			if (files->Length == 1)
-			{
-				//MessageBox::Show("Check1: " + on_off + " ");
-				std::string s_file = msclr::interop::marshal_as<std::string>(file);
-
-
-				on_off = fbom_sprawdzenie(s_file);
-				String^ filename = bom_filename = Path::GetFileName(file);
-				bom_filepath = Path::GetDirectoryName(file);
-				this->listBox1->Items->Add(filename);
-				//MessageBox::Show("Filename: " + filename + " "+on_off+" ");
-			}
-
-			//THEN DO WHATEVER YOU WANT TO EACH file in files
-		}
-		*/
-	}
-	private: System::Void listBox1_DragEnter(System::Object^ sender, System::Windows::Forms::DragEventArgs^ e)
-	{
-		if (e->Data->GetDataPresent(DataFormats::FileDrop, false) == true)
-		{
-			e->Effect = DragDropEffects::All;
-		}
-	}
-
+//		DRAG & DROP EVENTS
+//************************************************************
+	private:
+		System::Void listBox1_DragDrop(System::Object^ sender, System::Windows::Forms::DragEventArgs^ e);
+		System::Void listBox1_DragEnter(System::Object^ sender, System::Windows::Forms::DragEventArgs^ e);
+		System::Void textBox1_TextChanged(System::Object^ sender, System::EventArgs^ e);
 #pragma endregion
-	private: System::Void textBox1_TextChanged(System::Object^ sender, System::EventArgs^ e) {
-	}
 	};
 }

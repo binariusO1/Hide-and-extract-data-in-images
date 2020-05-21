8288db2c673e523bf6184f22924b3414f167860f
binariusO1 <ryszard1191@gmail.com>
2020-05-21 16:01:41 +0200

    up

--- WindowCoding.h
+++ WindowCoding.h
@@ -26,6 +26,15 @@ namespace Hideandextractdatainimages {
 				delete components;
 			}
 		}
+	private: System::Windows::Forms::TabControl^ tabControl1;
+	protected:
+	private: System::Windows::Forms::TabPage^ tabPage1;
+	private: System::Windows::Forms::TabPage^ tabPage2;
+	private: System::Windows::Forms::ListBox^ listBox1;
+	private: System::Windows::Forms::GroupBox^ groupBox1;
+	private: System::Windows::Forms::Label^ label2;
+	private: System::Windows::Forms::Label^ label1;
+
 
 	private:
 
@@ -35,12 +44,85 @@ namespace Hideandextractdatainimages {
 
 		void InitializeComponent(void)
 		{
-			this->components = gcnew System::ComponentModel::Container();
-			this->Size = System::Drawing::Size(300,300);
-			this->Text = L"WindowCoding";
-			this->Padding = System::Windows::Forms::Padding(0);
-			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			System::ComponentModel::ComponentResourceManager^ resources = (gcnew System::ComponentModel::ComponentResourceManager(WindowCoding::typeid));
+			this->tabControl1 = (gcnew System::Windows::Forms::TabControl());
+			this->tabPage1 = (gcnew System::Windows::Forms::TabPage());
+			this->tabPage2 = (gcnew System::Windows::Forms::TabPage());
+			this->listBox1 = (gcnew System::Windows::Forms::ListBox());
+			this->groupBox1 = (gcnew System::Windows::Forms::GroupBox());
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->label2 = (gcnew System::Windows::Forms::Label());
+			this->tabControl1->SuspendLayout();
+			this->tabPage1->SuspendLayout();
+			this->groupBox1->SuspendLayout();
+			this->SuspendLayout();
+			// 
+			// tabControl1
+			// 
+			this->tabControl1->Controls->Add(this->tabPage1);
+			this->tabControl1->Controls->Add(this->tabPage2);
+			resources->ApplyResources(this->tabControl1, L"tabControl1");
+			this->tabControl1->Name = L"tabControl1";
+			this->tabControl1->SelectedIndex = 0;
+			// 
+			// tabPage1
+			// 
+			this->tabPage1->Controls->Add(this->groupBox1);
+			resources->ApplyResources(this->tabPage1, L"tabPage1");
+			this->tabPage1->Name = L"tabPage1";
+			this->tabPage1->UseVisualStyleBackColor = true;
+			// 
+			// tabPage2
+			// 
+			resources->ApplyResources(this->tabPage2, L"tabPage2");
+			this->tabPage2->Name = L"tabPage2";
+			this->tabPage2->UseVisualStyleBackColor = true;
+			// 
+			// listBox1
+			// 
+			this->listBox1->FormattingEnabled = true;
+			resources->ApplyResources(this->listBox1, L"listBox1");
+			this->listBox1->Name = L"listBox1";
+			// 
+			// groupBox1
+			// 
+			this->groupBox1->Controls->Add(this->label2);
+			this->groupBox1->Controls->Add(this->label1);
+			this->groupBox1->Controls->Add(this->listBox1);
+			resources->ApplyResources(this->groupBox1, L"groupBox1");
+			this->groupBox1->Name = L"groupBox1";
+			this->groupBox1->TabStop = false;
+			// 
+			// label1
+			// 
+			resources->ApplyResources(this->label1, L"label1");
+			this->label1->Name = L"label1";
+			// 
+			// label2
+			// 
+			resources->ApplyResources(this->label2, L"label2");
+			this->label2->Name = L"label2";
+			// 
+			// WindowCoding
+			// 
+			this->AllowDrop = true;
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::None;
+			this->CausesValidation = false;
+			resources->ApplyResources(this, L"$this");
+			this->Controls->Add(this->tabControl1);
+			this->FormBorderStyle = System::Windows::Forms::FormBorderStyle::FixedSingle;
+			this->MaximizeBox = false;
+			this->Name = L"WindowCoding";
+			this->ShowIcon = false;
+			this->tabControl1->ResumeLayout(false);
+			this->tabPage1->ResumeLayout(false);
+			this->groupBox1->ResumeLayout(false);
+			this->groupBox1->PerformLayout();
+			this->ResumeLayout(false);
+
 		}
 #pragma endregion
+	private: System::Void textBox1_TextChanged(System::Object^ sender, System::EventArgs^ e) {
+	}
 	};
 }
